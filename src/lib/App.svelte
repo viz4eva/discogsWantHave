279373ee1666881f9d2ef008eb38bc518dad8834
ba067134 <eva-maria.haimerl@stud.uni-bamberg.de>
2024-10-24 14:47:16 +0200

    Add general explanation

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -7,10 +7,16 @@
 
 <h3>Discogs Most Wanted</h3>
 
+<p><a href="https://www.discogs.com/" target="_blank">Discogs</a> is a free, community-based data base for discographies and trading platform for records. It gives members the opportunity to mark the records they own and those they would like to own. This data gives insights on which are releases are rare within the Discogs community: those who many collectors want, but few already own. And which are owned by many but on few people's wantlist.</p>
+
+<p>This scatter plot visualization shows the correlations of want and have for the records from a selection of three user-generated release lists. Hover over a circle to see which release is represented by it.</p>
+
 <select bind:value={selectedList}>
+    <option value={discoClassics}>{discoClassics.name}</option>
     <option value={jungle}>{jungle.name}</option>
     <option value={rare}>{rare.name}</option>
-    <option value={discoClassics}>{discoClassics.name}</option>
 </select>
 
 <Scatterplot data={selectedList}/>
+
+<p>Data Source: <a href="https://www.discogs.com/developers/#" target="_blank">Discogs API</a></p>
