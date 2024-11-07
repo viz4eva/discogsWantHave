215aa1940c3c6e388aa4067ad93c4b3d9236d82d
ba067134 <eva-maria.haimerl@stud.uni-bamberg.de>
2024-11-07 10:34:54 +0100

    Bin visualization utils

--- src/lib/Scatterplot.svelte
+++ src/lib/Scatterplot.svelte
@@ -2,6 +2,7 @@
     import * as d3 from "d3";
     import { onMount } from "svelte";
     import Details from "./Details.svelte";
+    import {width, height, margin, textMargin, rectOverlayData, triangleOverlayData} from '$lib/utils/vis_utils';
     /**
      * @type {{ items: any[]; }}
      */
@@ -14,11 +15,6 @@
      * @type {boolean}
      */
     export let explorative;
-
-    const width = 600;
-    const height = width;
-    const margin = width / 10;
-    const textMargin = margin - 20;
 
     /**
      * @type {SVGSVGElement}
@@ -78,65 +74,9 @@
      */
     let zoomOut;
 
-    const triangleOverlayData = [
-        {
-            path: `${margin},0 ${width},0 ${margin},${height - margin}`,
-            fill: "#FFAE85",
-            text: "More people have it",
-            textX: margin + 10,
-            textY: 30,
-        },
-        {
-            path: `${margin},${height - margin} ${width},${height - margin} ${width},0`,
-            fill: "#1C29E1",
-            text: "More people want it",
-            textX: width / 2 + 20,
-            textY: height - margin - 10,
-        },
-    ];
+   
 
-    const rectOverlayData = [
-        {
-            x: margin,
-            y: 0,
-            width: width / 2 - margin + 10,
-            height: height / 2 - 10,
-            fill: "#00509d",
-            textX: width * 0.25 - margin,
-            textY: height * 0.25,
-            text: "saturated",
-        },
-        {
-            x: width / 2 + 10,
-            y: 0,
-            width: width / 2,
-            height: height / 2 - 10,
-            fill: "#ffd500",
-            textX: width * 0.75 - margin,
-            textY: height * 0.25,
-            text: "popular",
-        },
-        {
-            x: margin,
-            y: height / 2 - 10,
-            width: width / 2 - margin + 10,
-            height: height / 2 - margin + 10,
-            fill: "#ddd",
-            textX: width * 0.25 - 20,
-            textY: height * 0.75,
-            text: "basic",
-        },
-        {
-            x: width / 2 + 10,
-            y: height / 2 - 10,
-            width: width / 2,
-            height: height / 2 - margin + 10,
-            fill: "#eb5e28",
-            textX: width * 0.75 - margin - 40,
-            textY: height * 0.75,
-            text: "super rare",
-        },
-    ];
+    
 
     function recalcVisUtils() {
         releases = data.items.map(
---
+++ src/lib/utils/vis_utils.js
@@ -0,0 +1,64 @@
+export const width = 600;
+export const height = width;
+export const margin = width / 10;
+export const textMargin = margin - 20;
+
+export const rectOverlayData = [
+    {
+        x: margin,
+        y: 0,
+        width: width / 2 - margin + 10,
+        height: height / 2 - 10,
+        fill: "#00509d",
+        textX: width * 0.25 - margin,
+        textY: height * 0.25,
+        text: "saturated",
+    },
+    {
+        x: width / 2 + 10,
+        y: 0,
+        width: width / 2,
+        height: height / 2 - 10,
+        fill: "#ffd500",
+        textX: width * 0.75 - margin,
+        textY: height * 0.25,
+        text: "popular",
+    },
+    {
+        x: margin,
+        y: height / 2 - 10,
+        width: width / 2 - margin + 10,
+        height: height / 2 - margin + 10,
+        fill: "#ddd",
+        textX: width * 0.25 - 20,
+        textY: height * 0.75,
+        text: "basic",
+    },
+    {
+        x: width / 2 + 10,
+        y: height / 2 - 10,
+        width: width / 2,
+        height: height / 2 - margin + 10,
+        fill: "#eb5e28",
+        textX: width * 0.75 - margin - 40,
+        textY: height * 0.75,
+        text: "super rare",
+    },
+];
+
+export const triangleOverlayData = [
+    {
+        path: `${margin},0 ${width},0 ${margin},${height - margin}`,
+        fill: "#FFAE85",
+        text: "More people have it",
+        textX: margin + 10,
+        textY: 30,
+    },
+    {
+        path: `${margin},${height - margin} ${width},${height - margin} ${width},0`,
+        fill: "#1C29E1",
+        text: "More people want it",
+        textX: width / 2 + 20,
+        textY: height - margin - 10,
+    },
+];
